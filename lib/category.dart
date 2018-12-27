2aed9aca1b668c577ddda34d5f267fe56ce1020a
shivamvk <shivam.vk529@gmail.com>
2018-12-27 19:55:08 +0530

    add backdrop.dart for smooth UI/Ux

--- lib/category.dart
+++ lib/category.dart
@@ -1,87 +1,27 @@
 import 'package:flutter/material.dart';
-import 'converter_route.dart';
+import 'package:meta/meta.dart';
+
 import 'unit.dart';
 
-final _rowHeight = 75.0;
-final _borderRadius = BorderRadius.circular(_rowHeight / 2);
-
-class Category extends StatelessWidget {
+/// A [Category] keeps track of a list of [Unit]s.
+class Category {
   final String name;
   final ColorSwatch color;
-  final IconData iconLocation;
   final List<Unit> units;
+  final IconData iconLocation;
 
+  /// Information about a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), a list of its
+  /// its color for the UI, units for conversions (e.g. 'Millimeter', 'Meter'),
+  /// and the icon that represents it (e.g. a ruler).
   const Category({
-    Key key,
     @required this.name,
     @required this.color,
-    @required this.iconLocation,
     @required this.units,
+    @required this.iconLocation,
   })  : assert(name != null),
         assert(color != null),
-        assert(iconLocation != null),
         assert(units != null),
-        super(key: key);
-
-  void _navigateToConverter(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute<Null>(
-      builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 0.0,
-            title: Text(
-              name,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            centerTitle: true,
-            backgroundColor: Colors.white,
-          ),
-          body: ConverterRoute(
-            color: color,
-            name: name,
-            units: units,
-          ),
-          resizeToAvoidBottomPadding: false,
-        );
-      },
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: _borderRadius,
-          highlightColor: color['highlight'],
-          splashColor: color['splash'],
-          onTap: () => _navigateToConverter(context),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(8.0, 3.0, 8.0, 21.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 40.0,
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+        assert(iconLocation != null);
 }
--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'backdrop.dart';
 import 'category.dart';
+import 'category_tile.dart';
 import 'unit.dart';
-
-final _backgroundColor = Colors.white;
+import 'unit_converter.dart';
 
 class CategoryRoute extends StatefulWidget {
   const CategoryRoute();
@@ -13,6 +14,8 @@ class CategoryRoute extends StatefulWidget {
 }
 
 class _CategoryRouteState extends State<CategoryRoute> {
+  Category _defaultCategory;
+  Category _currentCategory;
   final _categories = <Category>[];
   static const _categoryNames = <String>[
     'Length',
@@ -64,22 +67,42 @@ class _CategoryRouteState extends State<CategoryRoute> {
   void initState() {
     super.initState();
     for (var i = 0; i < _categoryNames.length; i++) {
-      _categories.add(Category(
+      var category = Category(
         name: _categoryNames[i],
         color: _baseColors[i],
         iconLocation: Icons.cake,
         units: _retrieveUnitList(_categoryNames[i]),
-      ));
+      );
+      if (i == 0) {
+        _defaultCategory = category;
+      }
+      _categories.add(category);
     }
   }
 
+  /// Function to call when a [Category] is tapped.
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
+
+  /// Makes the correct number of rows for the list view.
+  ///
+  /// For portrait, we use a [ListView].
   Widget _buildCategoryWidgets() {
     return ListView.builder(
-      itemBuilder: (BuildContext context, int index) => _categories[index],
+      itemBuilder: (BuildContext context, int index) {
+        return CategoryTile(
+          category: _categories[index],
+          onTap: _onCategoryTap,
+        );
+      },
       itemCount: _categories.length,
     );
   }
 
+  /// Returns a list of mock [Unit]s.
   List<Unit> _retrieveUnitList(String categoryName) {
     return List.generate(10, (int i) {
       i += 1;
@@ -92,28 +115,24 @@ class _CategoryRouteState extends State<CategoryRoute> {
 
   @override
   Widget build(BuildContext context) {
-    final listView = Container(
-      color: Colors.grey[50],
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    final listView = Padding(
+      padding: EdgeInsets.only(
+        left: 8.0,
+        right: 8.0,
+        bottom: 48.0,
+      ),
       child: _buildCategoryWidgets(),
     );
 
-    final appBar = AppBar(
-      elevation: 0.0,
-      title: Text(
-        'Unit Converter',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 30.0,
-        ),
-      ),
-      centerTitle: true,
-      backgroundColor: _backgroundColor,
-    );
-
-    return Scaffold(
-      appBar: appBar,
-      body: listView,
+    return Backdrop(
+      currentCategory:
+      _currentCategory == null ? _defaultCategory : _currentCategory,
+      frontPanel: _currentCategory == null
+          ? UnitConverter(category: _defaultCategory)
+          : UnitConverter(category: _currentCategory),
+      backPanel: listView,
+      frontTitle: Text('Unit Converter'),
+      backTitle: Text('Select a Category'),
     );
   }
 }
